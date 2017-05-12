cc0c604f6626bdf90acc5102c25dde12c329a8af
XabiAlonso86 <xaviucm@gmail.com>
2017-05-12 11:51:05 +0100

    Eliminado audited.py

--- sistema/operaciones.py
+++ sistema/operaciones.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 import subprocess
+import re
+# Método para crear directorio de xauditor
 def createMainPath(mainPath):
     print ("<INFO>: Creando directorio de la aplicación en " + mainPath)
     subprocess.check_output("mkdir " + mainPath, shell=True)
 
+# Método para crear directorios para el análisis de una Dirección IP
 def createPath(mainPath, ip_address):
     print ("<INFO>: Creando directorios para IP " + ip_address)
-    subprocess.check_output("mkdir " + mainPath + "/" + ip_address, shell=True)
+    subprocess.check_output("mkdir " + mainPath + ip_address, shell=True)
+
+# Método para validar una IP
+def validIP(ip):
+    return re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",ip)
--- xauditor.py
+++ xauditor.py
@@ -4,44 +4,85 @@
 import os
 import sys
 import subprocess
+import multiprocessing
 # Imports de paquetes propios
 import sistema.operaciones as sistema
 from classes.auditedip import AuditedIP
 
+# Comprobación de parámetros
 print ("xauditor alpha 0.1")
 if len (sys.argv) < 2:
     print ("<ERROR>: Hay que introducir una dirección IP al menos")
     sys.exit()
 
-# Comienzo del programa
-if __name__=='__main__':
+# Función para multiproceso
+def multProc(targetin, scanip, port):
+    jobs = []
+    p = multiprocessing.Process(target=targetin, args=(scanip,port))
+    jobs.append(p)
+    p.start()
+    return
+
+# Función de escaneo NMAP
+def nmapScan(ip_address, mainPath):
+    nmapCmd = "sudo nmap -sV -O %s -oX '%s%s/%s.nmap'" % (ip_address,mainPath,ip_address,ip_address)
+    print ("<INFO>: Escaneo nmap de versiones " + nmapCmd + " sobre " + ip_address)
     try:
-        # Variables
-        hilos = []
-        # Recogemos IPs
-        targets = sys.argv
-        # Quitamos el primer objeto porque es el nombre del programa
-        targets.pop(0)
-
-        # Establecemos MainPath y creamos si es necesario
-        mainPath = "%s/xauditor/" % (os.environ['HOME'])
-        if not os.path.exists(mainPath):
-            sistema.createMainPath(mainPath)
-        # Recorremos cada IP obtenida y vamos ejecutando análisis
-        for scanip in targets:
-            try:
-                ip = AuditedIP(scanip, mainPath)
-                ip.setName('Thread_' + scanip)
-                hilos.append(ip)
-            # Excepción en caso de IP mal formateada
-            except ValueError:
-                print ("<ERROR>: IP " + scanip + " tiene un formato incorrecto")
-
-        # Corremos Hilos y esperamos a que terminen
-        for hilo in hilos:
-            hilo.start()
-        for hilo in hilos:
-            hilo.join()
+        results = subprocess.check_output(nmapCmd, shell=True)
     # Capturamos Cierre de la aplicación por el usuario
     except KeyboardInterrupt:
-        print "<ERROR>: Interrumpido por el usuario"
+        print "<ERROR>: Escaneo TCP Version sobre " + ip_address + " interrumpido por el usuario."
+    print (results)
+
+    # Lanzamos UDPScan (De momento no, que no tiene mucho sentido)
+    #p = multiprocessing.Process(target=udpScan, args=(ip_address, mainPath))
+    #p.start()
+
+# Función de escaneo NMAP a los puertos UDP
+def udpScan(ip_address, mainPath):
+    print "<INFO>: Escaneo UDP sobre " + ip_address
+    nmapCmd = "sudo nmap -vv -Pn -A -sC -sU -T 4 --top-ports 200 -oX '%s%s/udp_%s.nmap' %s"  % (mainPath,ip_address,ip_address,ip_address)
+    print "<INFO>:" + nmapCmd
+    try:
+        udpscan_results = subprocess.check_output(nmapCmd, shell=True)
+    # Capturamos Cierre de la aplicación por el usuario
+    except KeyboardInterrupt:
+        print "<ERROR>: Escaneo UDP sobre " + ip_address + " interrumpido por el usuario."
+
+    print "<INFO>: Terminado escaneo UDP para " + ip_address
+    print udpscan_results
+    print "<INFO>: Escaneo UDP unicornscan sobre " + ip_address
+    unicornCmd = "unicornscan -mU -v -I %s > '%s/%s/unicordn_udp_%s.txt'" % (ip_address,mainPath, ip_address, ip_address)
+    #unicornscan_results = subprocess.check_output(unicornCmd, shell=True)
+    print "<INFO>: unicornscan finalizado sobre " + ip_address
+
+
+# Comienzo del programa
+if __name__=='__main__':
+    # Recogemos IPs de los argumentos
+    targets = sys.argv
+    # Quitamos el primer objeto porque es el nombre del programa
+    targets.pop(0)
+
+    # Creamos directorio de la aplicación si es necesario
+    mainPath = "%s/xauditor/" % (os.environ['HOME'])
+    if not os.path.exists(mainPath):
+        sistema.createMainPath(mainPath)
+    # Recorremos cada IP obtenida y vamos ejecutando análisis de reco
+    for scanip in targets:
+        if sistema.validIP(scanip) is not None:
+            # Creamos directorios para la IP
+            dirs = os.listdir(mainPath)
+            if not scanip in dirs:
+                sistema.createPath(mainPath, scanip)
+            # Proceso para realizar escaneos NMAP
+            p = multiprocessing.Process(target=nmapScan, args=(scanip,mainPath))
+            p.start()
+        else:
+            print ("<ERROR>: IP " + scanip + " tiene un formato incorrecto")
+
+        # Corremos Hilos y esperamos a que terminen
+        #for hilo in hilos:
+        #    hilo.start()
+        #for hilo in hilos:
+        #    hilo.join()
