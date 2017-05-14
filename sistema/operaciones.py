820161166e891ae70178453f96351b2bdc84298a
XabiAlonso86 <xaviucm@gmail.com>
2017-05-14 12:22:25 +0100

    Añadido logger

--- sistema/operaciones.py
+++ sistema/operaciones.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 import subprocess
 import re
+import logging
 # Paquetes locales
 import classes.servicio as srv
 
+module_logger = logging.getLogger("xauditor.operaciones")
+
 # Método para crear directorio de xauditor
 def createMainPath(mainPath):
-    print ("<INFO>: Creando directorio de la aplicación en " + mainPath)
+    module_logger.info("Creando directorio de la aplicación en %s",mainPath)
     subprocess.check_output("mkdir " + mainPath, shell=True)
 
 # Método para crear directorios para el análisis de una Dirección IP
-def createPath(mainPath, ip_address):
-    print ("<INFO>: Creando directorios para IP " + ip_address)
-    subprocess.check_output("mkdir " + mainPath + ip_address, shell=True)
+def createPath(mainPath, ip_address):    
+    module_logger.info("Creando directorios para IP %s",ip_address)
+    subprocess.check_output("mkdir " + mainPath + "/" + ip_address, shell=True)
 
 # Método para validar una IP
 def validIP(ip):
     return re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",ip)
 
-# Método que analiza la línea NMPA para un puerto
+# Método que analiza la línea NMAP para un puerto
 def processPortLine(line, services):
     estado = ''
     # Dejamos la línea sólo con espacios
@@ -57,5 +60,4 @@ def analyzeNMAP(result):
     for nombre, servicio in services.items():
         print nombre
     print len(services)
-    # Devolvemos lista de servicios
-    return services
+    # Devolvemos
--- xauditor.py
+++ xauditor.py
@@ -5,6 +5,8 @@ import os
 import sys
 import subprocess
 import multiprocessing
+import logging
+import time
 # Imports de paquetes propios
 import sistema.operaciones as oper
 
@@ -24,15 +26,15 @@ def multProc(targetin, scanip, port):
 
 # Función de escaneo NMAP
 def nmapScan(ip_address, mainPath):
-    nmapCmd = "sudo nmap -sV -O %s -oN '%s%s/%s.nmap'" % (ip_address,mainPath,ip_address,ip_address)
-    print "<INFO>: Escaneo nmap de versiones (" + nmapCmd + ") sobre " + ip_address
+    nmapCmd = "sudo nmap -sV -O %s -oN '%s/%s/%s.nmap'" % (ip_address,mainPath,ip_address,ip_address)
+    logger.info("Escaneo nmap de versiones (%s) sobre %s",nmapCmd,ip_address)
     try:
         results = subprocess.check_output(nmapCmd, shell=True)
         # Analizamos resultados y obtenemos lista de servicios disponibles
         services = oper.analyzeNMAP(results)
     # Capturamos Cierre de la aplicación por el usuario
     except KeyboardInterrupt:
-        print "<ERROR>: Escaneo TCP Version sobre " + ip_address + " interrumpido por el usuario."
+        logger.error("Escaneo TCP Version sobre %s interrumpido por el usuario.",ip_address)
     #print (results)
 
     # Lanzamos UDPScan
@@ -41,21 +43,21 @@ def nmapScan(ip_address, mainPath):
 
 # Función de escaneo NMAP a los puertos UDP
 def udpScan(ip_address, mainPath):
-    print "<INFO>: Escaneo UDP sobre " + ip_address
-    nmapCmd = "sudo nmap -vv -Pn -A -sC -sU -T 4 --top-ports 200 -oN '%s%s/udp_%s.nmap' %s"  % (mainPath,ip_address,ip_address,ip_address)
-    print "<INFO>: Escaneo UDP (" + nmapCmd + ") sobre " + ip_address
+    logger.info("Escaneo UDP sobre %s",ip_address)
+    nmapCmd = "sudo nmap -vv -Pn -A -sC -sU -T 4 --top-ports 200 -oN '%s/%s/udp_%s.nmap' %s"  % (mainPath,ip_address,ip_address,ip_address)
+    logger.info("Escaneo nmap UDP (%s) sobre %s",nmapCmd,ip_address)
     try:
         udpscan_results = subprocess.check_output(nmapCmd, shell=True)
     # Capturamos Cierre de la aplicación por el usuario
     except KeyboardInterrupt:
-        print "<ERROR>: Escaneo UDP sobre " + ip_address + " interrumpido por el usuario."
+        logger.error("Escaneo UDP sobre %s interrumpido por el usuario.",ip_address)
 
-    print "<INFO>: Terminado escaneo UDP para " + ip_address
+    logger.info("Terminado escaneo UDP para %s",ip_address)
     print udpscan_results
-    print "<INFO>: Escaneo UDP unicornscan sobre " + ip_address
+    logger.info("Escaneo UDP unicornscan sobre %s",ip_address)
     unicornCmd = "unicornscan -mU -v -I %s > '%s/%s/unicordn_udp_%s.txt'" % (ip_address,mainPath, ip_address, ip_address)
     #unicornscan_results = subprocess.check_output(unicornCmd, shell=True)
-    print "<INFO>: unicornscan finalizado sobre " + ip_address
+    logger.info("Unicornscan finalizado sobre %s",ip_address)
 
 
 # Comienzo del programa
@@ -65,10 +67,31 @@ if __name__=='__main__':
     # Quitamos el primer objeto porque es el nombre del programa
     targets.pop(0)
 
+    time.strftime("%d_%m_%Y")
+
     # Creamos directorio de la aplicación si es necesario
-    mainPath = "%s/xauditor/" % (os.environ['HOME'])
+    mainPath = "%s/xauditor/%s" % (os.environ['HOME'],time.strftime("%d_%m_%Y"))
     if not os.path.exists(mainPath):
         oper.createMainPath(mainPath)
+
+    # Creamos el logger
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger("xauditor")
+
+    # Creamos file handler para guardar el log en un fichero
+    fh = logging.FileHandler(mainPath + '/xauditor@%s.log' % time.strftime("%H_%M_%S"))
+    fh.setLevel(logging.DEBUG)
+    # Creamos console handle para mostrar log en la consola
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    # Creamos formato de log y lo añadimos a los handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    # Añadir al logger los handlers para que se encargue de generar los logs
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
     # Recorremos cada IP obtenida y vamos ejecutando análisis de reco
     for scanip in targets:
         if oper.validIP(scanip) is not None:
@@ -80,10 +103,4 @@ if __name__=='__main__':
             p = multiprocessing.Process(target=nmapScan, args=(scanip,mainPath))
             p.start()
         else:
-            print ("<ERROR>: IP " + scanip + " tiene un formato incorrecto")
-
-        # Corremos Hilos y esperamos a que terminen
-        #for hilo in hilos:
-        #    hilo.start()
-        #for hilo in hilos:
-        #    hilo.join()
+            logger.error ("<ERROR>: IP %s tiene un formato incorrecto",scanip)
