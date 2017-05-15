83fe9cdd228731c3b462e67ed7b2186b89a81bf6
xabialonso86 <xaviucm@gmail.com>
2017-05-15 21:32:01 +0200

    Conseguido analizar servicios HTTP. Falta preparar su escaneo

--- classes/analyzedIP.py
+++ classes/analyzedIP.py
@@ -22,92 +22,68 @@ class AnalizedIP:
 
     # Método para analizar una IP
     def analyze(self):
+        # Lanzamos dos hilos asincronos porque UDP tarda bastante...
         with ThreadPoolExecutor(max_workers=2) as executor:
-            resultNmapScan = executor.submit(nmapScan, self.ip,self.folder)
-            resultUDPScan= executor.submit(udpScan, self.ip,self.folder)
+            resultNmapScan = executor.submit(nmapScan, self.ip,self.folder,self.servicios)
+            resultUDPScan= executor.submit(udpScan, self.ip,self.folder,self.serviciosUDP)
 
-        self.servicios = resultNmapScan.result()
-        self.serviciosUDP = resultUDPScan.result()
+        #self.servicios = resultNmapScan.result()
+        #self.serviciosUDP = resultUDPScan.result()
 
         # Obtenidos los servicios de la dirección IP hay que analizarlos
         # y ver si se pueden lanzar nuevos comandos
         print ("Servicios dirección %s: %s" % (self.ip, len(self.servicios)))
         print ("Servicios UDP dirección %s: %s" % (self.ip, len(self.serviciosUDP)))
-        self.analyzeServices()
+        #analyzeServices()
 
-    def analyzeServices(self):
-        # Lo primero es ver si tenemos más de un serivio (servicio OS siempre está)
-        if len(self.servicios) == 1:
-            logger.info("No se han encontrado servicios para la IP %s. Revise el análisis de NMAP o comience a investigar por su cuenta." % (self.ip))
-        else:
-            for nombre, servicio in self.servicios.items():
-                # Servicio HTTP
-                if (nombre == "http") or (nombre == "http-proxy") or (nombre == "http-alt") or (nombre == "http?"):
-                    for port in servicio.puertos:
-                        print ("Encontrado servicio http para ip %s en puerto %s" % (self.ip,port))
+def analyzeServices(ip, servicios):
+    # Lo primero es ver si tenemos más de un serivio (servicio OS siempre está)
+    if len(servicios) == 1:
+        logger.info("No se han encontrado servicios para la IP %s. Revise el análisis de NMAP o comience a investigar por su cuenta." % (ip))
+    else:
+        for nombre, servicio in servicios.items():
+            # Servicio HTTP
+            if (nombre == "http") or (nombre == "http-proxy") or (nombre == "http-alt") or (nombre == "http?"):
+                for port in servicio.puertos:
+                    logger.info("Encontrado servicio http para ip %s en puerto %s" % (ip,port))
+                    print ("Encontrado servicio http para ip %s en puerto %s" % (ip,port))
 
-        if len(self.serviciosUDP) == 1:
-            logger.info("No se han encontrado servicios UDP para la IP %s. Revise el análisis de NMAP o comience a investigar por su cuenta." % (self.ip))
-
-        # Primero miramos los servicios que no son UDP
-
-
-
-
-# Función para multiproceso
-def multProc(targetin,ip,folder):
-    p = multiprocessing.Process(target=targetin, args=(ip,folder))
-    jobs.append(p)
-    return
 
 # Función de escaneo NMAP
-def nmapScan(ip,folder):
+def nmapScan(ip,folder,servicios):
     servicios = {}
-    nmapCmd = "sudo nmap -sV -O %s -oN '%s/%s@%s.nmap'" % (ip,folder,ip,time.strftime("%H_%M_%S"))
+    nmapCmd = "nmap -sV -O %s -oN '%s/%s@%s.nmap'" % (ip,folder,ip,time.strftime("%H_%M_%S"))
     logger.info("Escaneo nmap de versiones sobre %s",ip)
     logger.debug("Comando nmapScan: %s",nmapCmd)
     try:
         results = subprocess.getoutput(nmapCmd)
         # Analizamos resultados y obtenemos lista de servicios disponibles
-        return analyzeNMAPResult(results)
-        #for nombre, servicio in servicios.items():
-        #    print (nombre)
-        #print (len(servicios))
+        servicios = analyzeNMAPResult(results)
+        analyzeServices(ip,servicios)
     # Capturamos Cierre de la aplicación por el usuario
     except KeyboardInterrupt:
         logger.exception("Escaneo TCP Version sobre %s interrumpido por el usuario.",ip)
     #print (results)
-
-    # Lanzamos UDPScan
-    #p = multiprocessing.Process(target=udpScan, args=(ip_address, mainPath))
-    #p.start()
-
-
+   
 # Función de escaneo NMAP a los puertos UDP
-def udpScan(ip,folder):
+def udpScan(ip,folder,servicios):
     servicios = {}
-    nmapCmd = "sudo nmap -vv -Pn -A -sC -sU -T 4 --top-ports 200 -oN '%s/udp_%s@%s.nmap' %s"  % (folder,ip,time.strftime("%H_%M_%S"),ip)
+    nmapCmd = "nmap -vv -Pn -A -sC -sU -T 4 --top-ports 200 -oN '%s/udp_%s@%s.nmap' %s"  % (folder,ip,time.strftime("%H_%M_%S"),ip)
     logger.info("Escaneo nmap UDP sobre %s",ip)
     logger.debug("Comando udpScan: %s",nmapCmd)
     try:
-        servicio = srv.Servicio("hola")
-        servicio.puertos.append("0")
-        servicio.versiones.append("0")
-        servicio.estados.append("0")
-        servicios["hola"] = servicio
-        return servicios
-        #udpscan_results = subprocess.getoutput(nmapCmd)
+        results = subprocess.getoutput(nmapCmd)
+        logger.info("Terminado escaneo UDP para %s",ip)
+        # Lanzamos unicornScan aunque no lo analicemos
+        logger.info("Escaneo UDP unicornscan sobre %s",ip)
+        unicornCmd = "unicornscan -mU -v -I %s > '%s/unicordn_udp_%s@%s.txt'" % (ip,folder,ip,time.strftime("%H_%M_%S"))
+        logger.debug("Comando unicornscan: %s",unicornCmd)
+        unicornscan_results = subprocess.getoutput(unicornCmd)
+        logger.info("Unicornscan finalizado sobre %s",ip)
+        return analyzeNMAPResult(results)
     # Capturamos Cierre de la aplicación por el usuario
     except KeyboardInterrupt:
-        logger.error("Escaneo UDP sobre %s interrumpido por el usuario.",ip)
-
-    #logger.info("Terminado escaneo UDP para %s",ip.address)
-    #print (udpscan_results)
-    #logger.info("Escaneo UDP unicornscan sobre %s",ip.address)
-    #unicornCmd = "unicornscan -mU -v -I %s > '%s/%s/unicordn_udp_%s@%s.txt'" % (ip,folder, ip, ip,time.strftime("%HH_%M_%S"))
-    #logger.debug("Comando unicornscan: %s",unicornCmd)
-    #unicornscan_results = subprocess.getoutput(unicornCmd)
-    #logger.info("Unicornscan finalizado sobre %s",ip)
+        logger.error("Escaneo UDP sobre %s interrumpido por el usuario.",ip)    
 
 # Método que analiza los resultados línea a línea de un test NMAP
 def analyzeNMAPResult(result):
